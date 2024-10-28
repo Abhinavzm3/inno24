30dcd1bfdf302fa4cd07df46f64fb757dd351580
Hemant Raj <coder.hemantraj05@gmail.com>
2024-10-28 23:20:53 +0530

    fixed the issue of gaps

--- frontend/src/layout/app-layout.jsx
+++ frontend/src/layout/app-layout.jsx
@@ -5,13 +5,10 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <main className="min-h-screen container">
+      <main className="min-h-screen">
         <Header />
         <Outlet />
       </main>
-      <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by DeVdoot
-      </div>
     </div>
   );
 };
