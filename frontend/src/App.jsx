aff625e8adf5afe9d170ff232663364ad5956b6a
Hemant Raj <coder.hemantraj05@gmail.com>
2024-10-28 11:46:00 +0530

    Installed Shadcn components and gave dark theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import './App.css'
-import Navbar from './components/shared/Navbar';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider } from "./components/theme-provider";
+import Home from "./pages/Home";
+import AppLayout from "./layout/app-layout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-
   return (
     <>
-    
-      <Navbar/>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
