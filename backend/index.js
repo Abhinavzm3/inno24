81690fdf5cccbabbae216902d803b34e321be784
Hemant Raj <coder.hemantraj05@gmail.com>
2024-11-03 00:55:18 +0530

    configuration for vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    // origin:'http://localhost:5173',
+    origin:'http://localhost:5173',
     origin:'https://stark-connect.netlify.app',
+    origin:'https://stark-connect-seven.vercel.app/',
     credentials:true
 }
 
