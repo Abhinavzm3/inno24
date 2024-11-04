5dad586bff5cf33364c700d6f289bfd606068814
Abhinav Kumar <136048324+Abhinavzm3@users.noreply.github.com>
2024-11-04 01:29:44 -0500

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -99,7 +99,7 @@ export const login = async (req, res) => {
     return res
       .status(200)
       .cookie("token", token, {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
+        maxAge:365* 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'lax',
       })
